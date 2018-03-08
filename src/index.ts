57f131767ba0f9ea5b98b30abb5b3c2e31395eb3
Cameron Batt <vespertilian@gmail.com>
2018-03-08 12:58:34 +1100

    refactor(package.json): improve scripts names in package.json and stop create-json-from-handlebars-t

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export {getIANATzData} from './get-iana-tz-data/get-iana-tz-data';
 export {loadIANATzJsonFile} from './load-iana-tz-json-file/load-iana-tz-json-file';
+export {createJSONFromHandlebarsTemplatesAndZoneData} from './create-json-from-handlebars-templates/create-json-from-handlebars-templates'
